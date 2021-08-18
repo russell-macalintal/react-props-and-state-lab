1615353a3af7122251a03cdd5919278bb7c59560
russell-macalintal <russell-macalintal@gmail.com>
2021-08-18 03:18:27 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,44 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (event) => {
+    this.setState({
+      filters: {
+        type: event.target.value
+      }
+    })
+  }
+
+  onFindPetsClick = () => {
+    if(this.state.filters.type === "all"){
+      fetch('/api/pets')
+      .then( response => response.json() )
+      .then( json => this.setState({
+        pets: json
+      }))
+    }else{
+      fetch(`/api/pets?type=${this.state.filters.type}`)
+      .then( response => response.json() )
+      .then( json => this.setState({
+        pets: json
+      }))
+    }
+  }
+
+  // Custom search funtion
+  // 1. Map the array and iterate through the elements
+  // 2. If element is a match, use the spread operator (...elem) in order to copy all key:value pairs within the object element and provide a second argument to overwrite a key with its new value. Then return the edited element into the array.
+  // 3. If element is not a match, simply return the element as part of the map function to be included back into the array
+  // 4. Use setState() to update the state with the new array (edited)
+  onAdoptPet = (id) => {
+    let pets = this.state.pets.map( pet => {
+      return pet.id === id ? {...pet, isAdopted: true} : pet;
+    })
+    this.setState({
+      pets: pets
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +62,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
